b615fa00c3957a701e84bb6d808fa4407ad31641
sanislearning <sanjaybiju10@gmail.com>
2025-05-26 01:43:51 +0530

    Basic python script is done

---
+++ main.py
@@ -0,0 +1,15 @@
+import re
+from collections import defaultdict
+def parse_whatsapp_chat(file_path):
+        pattern = r'^(\d{1,2}/\d{1,2}/\d{2,4}), \d{1,2}:\d{2}\s?(?:AM|PM)? - (.*?):'
+        counts=defaultdict(int)
+
+        with open(file_path,'r',encoding='utf-8') as f:
+                for line in f:
+                        match=re.match(pattern,line)
+                        if match:
+                                sender=match.group(2)
+                                counts[sender]+=1
+        return dict(counts)
+chatinfo=parse_whatsapp_chat() #add your file path here
+print(chatinfo)
